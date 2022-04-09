5ca1ed30db495b9ab4fd9b5e09241a6cf286fabc
N4vpn <101988742+N4vpn@users.noreply.github.com>
2022-04-10 01:50:20 +0630

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import asyncio
+from pytgcalls import idle
+from driver.n4 import call_py, bot, user
+
+
+async def start_bot():
+    await bot.start()
+    print("[INFO]: BOT & UBOT CLIENT STARTED !!")
+    await call_py.start()
+    print("[INFO]: PY-TGCALLS CLIENT STARTED !!")
+    await idle()
+    print("[INFO]: STOPPING BOT & USERBOT")
+    await bot.stop()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())
